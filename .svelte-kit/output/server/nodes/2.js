f2306cc5d61d280d14162609ac4cf6650726d04e
andreistaicu1 <55329808+andreistaicu1@users.noreply.github.com>
2025-09-09 15:37:39 -0500

    add nojekyll

--- .svelte-kit/output/server/nodes/2.js
+++ .svelte-kit/output/server/nodes/2.js
@@ -3,6 +3,6 @@
 export const index = 2;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.39332e61.js","_app/immutable/chunks/index.8528c4bc.js"];
+export const imports = ["_app/immutable/nodes/2.b8bcd8cd.js","_app/immutable/chunks/index.8528c4bc.js"];
 export const stylesheets = [];
 export const fonts = [];
